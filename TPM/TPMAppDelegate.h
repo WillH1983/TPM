96227995c884275b698cca6cde2d12f073847823
Will Hindenburg <WillH1983@yahoo.com>
2012-07-29 18:10:42 -0500

    Initial Commit

---
+++ TPM/TPMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TPMAppDelegate.h
+//  TPM
+//
+//  Created by Will Hindenburg on 7/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TPM/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TPM
+//
+//  Created by Will Hindenburg on 7/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TPMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TPMAppDelegate class]));
+    }
+}
