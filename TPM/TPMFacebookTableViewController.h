3dcd7ae15f5b19a0b18d87c6a703263a0cc201a4
Will Hindenburg <WillH1983@yahoo.com>
2012-08-01 01:09:04 -0500

    Started adding custom features for Tech Power Math Facebook page

---
+++ TPM/TPMFacebookTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TPMFacebookTableViewController.h
+//  TPM
+//
+//  Created by Will Hindenburg on 8/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FaceBookTableViewController.h"
+
+@interface TPMFacebookTableViewController : FaceBookTableViewController
+
+@end
---
+++ TPM/TPMFacebookTableViewController.m
@@ -0,0 +1,51 @@
+//
+//  TPMFacebookTableViewController.m
+//  TPM
+//
+//  Created by Will Hindenburg on 8/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TPMFacebookTableViewController.h"
+
+@interface TPMFacebookTableViewController ()
+
+@end
+
+@implementation TPMFacebookTableViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [[[self navigationController] navigationBar] setTintColor:[UIColor colorWithHue:0 saturation:0 brightness:0.30 alpha:1.0]];
+    
+}
+
+@end
