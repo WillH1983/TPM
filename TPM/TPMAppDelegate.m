7476f4d0e21dec7912ef69c059ae778e4ad8d184
Will Hindenburg <WillH1983@yahoo.com>
2012-08-13 21:47:51 -0500

    Starting working on advanced twitter functionality

--- TPM/TPMAppDelegate.m
+++ TPM/TPMAppDelegate.m
@@ -24,6 +24,7 @@
     facebook = [[Facebook alloc] initWithAppId:FACEBOOK_APP_ID andDelegate:nil];
     self.appConfiguration.facebookID = FACEBOOK_APP_ID;
     self.appConfiguration.facebookFeedToRequest = @"techpoweredmath";
+    self.appConfiguration.twitterUserNameToRequest = @"TheBlimpInc";
 
     application.statusBarStyle = UIStatusBarStyleBlackOpaque;
     return YES;
